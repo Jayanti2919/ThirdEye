93071102a37729c095b1a9b6daee5578c601fb1e
Jayanti2919 <jayanti2919@gmail.com>
2024-01-28 00:20:22 +0530

    set up mysql and mongodb

--- web2/index.js
+++ web2/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const {sqlConnect, mongoConnect} = require('./utils/connection');
+const Users = require("./models/users.model")
+const NewUsers = require("./models/newuser.model")
+const Videos = require("./models/video.model")
+
 dotenv.config();
 const app = express();
+const connection = sqlConnect();
+mongoConnect();
+
+const syncTables = async() => {
+    await Users.sync()
+    await NewUsers.sync()
+    await Videos.sync()
+}
+
+syncTables()
 
 app.get('/', (req, res) => {
   res.send('Listening');
@@ -10,5 +25,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
-  console.log('Web2 listening on port ', PORT);
+  console.log('Web2 listening on port', PORT);
 });
---
+++ web2/models/users.model.js
@@ -0,0 +1,33 @@
+const { sqlConnect } = require('../utils/connection');
+const { DataTypes } = require('sequelize');
+
+const connection = sqlConnect();
+
+const Users = connection.define('users', {
+    userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    channelName: {
+        type: DataTypes.STRING,
+    },
+    channelDesc: {
+        type: DataTypes.STRING,
+    },
+    subscriberCount: {
+        type: DataTypes.INTEGER,
+    },
+    profilePic: {
+        type: DataTypes.STRING,
+    },
+}, {
+    tableName: "users",
+    timestamps: false,
+});
+
+module.exports = Users;
