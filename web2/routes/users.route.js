3601bff4ece1dbaf2d7e2ae3c6d3e9e10efbab43
Jayanti2919 <jayanti2919@gmail.com>
2024-02-06 22:02:18 +0530

    added otp for login

--- web2/routes/users.route.js
+++ web2/routes/users.route.js
@@ -2,6 +2,8 @@ const User = require('../models/SQL/users.model');
 const sequelize = require('sequelize');
 const express = require('express');
 const router = express.Router();
+const { generateOTP, sendEmail } = require('../utils/functions');
+const { route } = require('./registration.route');
 
 router.route('/createUser').post(async(req, res)=>{
     const body = req.body;
@@ -70,6 +72,45 @@ router.route('/updateSubCount').put(async(req, res)=>{
         console.log(err);
         res.send(err);
     })
+})
+
+router.route('/loginOTP').post(async(req, res)=>{
+    const body = req.body;
+    const user = await User.findOne({ where: { email: body.email } });
+    if (!user) {
+        res.json({ message: "User not found" });
+    }
+    const otp = generateOTP();
+    await User.update(
+        {
+            otp: otp,
+        },
+        {
+            where: {
+                email: body.email,
+            },
+        }
+    ).then(async() => {
+        console.log("OTP updated successfully");
+        const response = await sendEmail(body.email, otp);
+        res.json({ message: response });
+    }).catch((err) => {
+        console.log(err);
+        res.json({ message: "Error updating OTP" });
+    });
+})
+
+router.route('/verifyloginOTP').post(async(req, res)=>{
+    const body = req.body;
+    const user = await User.findOne({ where: { email: body.email } });
+    if (!user) {
+        res.json({ message: "User not found" });
+    }
+    if (user.otp === body.otp) {
+        res.json({ message: "OTP verified successfully" });
+    } else {
+        res.json({ message: "Invalid OTP" });
+    }
 })
 
 module.exports = router;
