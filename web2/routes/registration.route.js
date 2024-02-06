2e44337defcff0dfeafbc01ece559890131e0cf1
Jayanti2919 <jayanti2919@gmail.com>
2024-02-06 18:58:37 +0530

    started recommender

--- web2/routes/registration.route.js
+++ web2/routes/registration.route.js
@@ -11,6 +11,34 @@ function generateOTP() {
   return otp.toString();
 }
 
+async function sendEmail(email, otp) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS | "",
+    to: email,
+    subject: "OTP for login in ThirdEye",
+    text: `Your OTP is ${otp}.`,
+  };
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error("Error sending email", err);
+        reject("Internal server error");
+      } else {
+        console.log("Email sent:", info.response);
+        resolve("OTP sent successfully");
+      }
+    });
+  });
+}
+
 router.route("/sendOTP").post(async (req, res) => {
   const body = req.body;
   const otp = generateOTP();
@@ -50,58 +78,44 @@ router.route("/sendOTP").post(async (req, res) => {
       });
   }
   if (flag === 1) {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: process.env.EMAIL_ADDRESS | "",
-      to: body.email,
-      subject: "OTP for login in ThirdEye",
-      text: `Your OTP is ${otp}.`,
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.error("Error sending email", err);
-        res.status(500).send("Internal server error");
-      } else {
-        console.log("Email sent:", info.response);
-        res.send("OTP sent successfully");
-      }
-    });
+    try {
+      const response = await sendEmail(body.email, otp);
+      console.log(response);
+      res.json({ message: response });
+    } catch (err) {
+      res.json({ message: "Error sending email" });
+    }
   } else {
     res.json({ message: "Error" });
   }
 });
 
 router.route("/verifyOTP").post(async (req, res) => {
-    const body = req.body;
-    const user = await RegistrationOTP.findOne({
-        where: {
+  const body = req.body;
+  const user = await RegistrationOTP.findOne({
+    where: {
+      email: body.email,
+    },
+  });
+  if (user) {
+    if (user.otp === body.otp) {
+      await Users.create({
         email: body.email,
-        },
-    });
-    if (user) {
-        if (user.otp === body.otp) {
-        await Users.create({
-            email: body.email,
-        }).then(() => {
-            console.log("User created");
-            res.send("OTP verified and user created successfully");
-        }).catch((err) => {
-            console.log(err);
-            res.send("OTP verified but error creating user");
+      })
+        .then(() => {
+          console.log("User created");
+          res.send("OTP verified and user created successfully");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send("OTP verified but error creating user");
         });
-        } else {
-            res.send("Invalid OTP");
-        }
     } else {
-        res.send("Email not found");
+      res.send("Invalid OTP");
     }
-})
+  } else {
+    res.send("Email not found");
+  }
+});
 
 module.exports = router;
