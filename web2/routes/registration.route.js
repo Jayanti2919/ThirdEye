e94c10538780064ff171298887ecac55736d31e1
Jayanti2919 <jayanti2919@gmail.com>
2024-02-06 10:16:36 +0530

    added nodemailer and otp sending

--- web2/routes/registration.route.js
+++ web2/routes/registration.route.js
@@ -1,46 +1,80 @@
-const sequelize = require('sequelize');
+const sequelize = require("sequelize");
 const RegistrationOTP = require("../models/SQL/registrationotp.model");
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 function generateOTP() {
-    const otp = Math.floor(100000 + crypto.randomInt(999999));
-    return otp.toString();
+  const otp = Math.floor(100000 + crypto.randomInt(999999));
+  return otp.toString();
 }
 
-router.route('/sendOTP').post(async(req, res)=>{
-    const body = req.body;
-    const otp = generateOTP();
-    const user = await RegistrationOTP.findOne({
+router.route("/sendOTP").post(async (req, res) => {
+  const body = req.body;
+  const otp = generateOTP();
+  var flag = 0;
+  const user = await RegistrationOTP.findOne({
+    where: {
+      email: body.email,
+    },
+  });
+  if (user) {
+    await RegistrationOTP.update(
+      {
+        otp: otp,
+      },
+      {
         where: {
-            email: body.email
-        }
+          email: body.email,
+        },
+      }
+    )
+      .then(() => {
+        flag = 1;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    await RegistrationOTP.create({
+      email: body.email,
+      otp: otp,
+    })
+      .then(() => {
+        flag = 1;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  if (flag === 1) {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      },
     });
-    if(user) {
-        await RegistrationOTP.update({
-            otp: otp
-        },{
-            where: {
-                email: body.email
-            }
-        }).then(()=>{
-            res.send("OTP updated successfully");
-        }).catch((err)=>{
-            console.log(err);
-            res.send(err);
-        })
-    }
-    else {
-        await RegistrationOTP.create({
-            email: body.email,
-            otp: otp
-        }).then(()=>{
-            res.send("OTP created successfully");
-        }).catch((err)=>{
-            console.log(err);
-            res.send(err);})
-    }
-})
+
+    const mailOptions = {
+      from: process.env.EMAIL_ADDRESS | "",
+      to: body.email,
+      subject: "OTP for login in ThirdEye",
+      text: `Your OTP is ${otp}.`,
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error("Error sending email", err);
+        res.status(500).send("Internal server error");
+      } else {
+        console.log("Email sent:", info.response);
+        res.send("OTP sent successfully");
+      }
+    });
+  } else {
+    res.json({ message: "Error" });
+  }
+});
 
 module.exports = router;
