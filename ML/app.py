a679450c65fdca7d6f2cddd460b1a6357bd742bd
NSinha025 <sinhanaman025@gmail.com>
2024-02-12 22:55:28 +0530

    SQLAlchemy implemented

--- ML/app.py
+++ ML/app.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-import mysql.connector
+# import mysql.connector
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/thirdeye'
+db = SQLAlchemy(app)
+
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+    videoHash = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    uploadDate = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    tags = db.Column(db.String(255))
+    genre = db.Column(db.String(255))
+    likeCount = db.Column(db.Integer)
+
+    # Connect to your MySQL database
+# db_connection = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='',
+#     database='thirdeye'
+# )
 
 @app.route('/')
 def index():
@@ -13,23 +36,25 @@ def index():
 @app.route('/recommendations', methods=['POST'])
 def get_recommendations():
     # Connect to your MySQL database
-    db_connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='thirdeye'
-    )
+    # db_connection = mysql.connector.connect(
+    #     host='localhost',
+    #     user='root',
+    #     password='',
+    #     database='thirdeye'
+    # )
+
+    movies = Video.query.all()
+    movies_df = pd.DataFrame([(movie.title, movie.uploadDate, movie.description, movie.tags, movie.genre, movie.likeCount) for movie in movies],
+                             columns=['title', 'uploadDate', 'description', 'tags', 'genre', 'likeCount'])
 
     # Fetch data from the database
-    query = "SELECT title, uploadDate, description, tags, genre, likeCount FROM videos;"
-    movies_df = pd.read_sql(query, con=db_connection)
+    # query = "SELECT title, uploadDate, description, tags, genre, likeCount FROM videos;"
+    # movies_df = pd.read_sql(query, con=db_connection)
 
     # Close the database connection
-    db_connection.close()
+    # db_connection.close()
 
-    # Content-based filtering using TF-IDF
-    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
-    genres_tags_matrix = tfidf_vectorizer.fit_transform(movies_df['genre'] + ' ' + movies_df['tags'])
+    
 
     # Process user input from the form
     user_preferences = {
@@ -43,6 +68,10 @@ def get_recommendations():
     user_genres_tags = ', '.join([genre for genre, count in user_preferences['genresWatched'].items() for _ in range(count)] + user_preferences['tagsWatched'])
     user_data = pd.DataFrame({'genre': [user_genres_tags], 'tags': [user_genres_tags]})
 
+    # Content-based filtering using TF-IDF
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+    genres_tags_matrix = tfidf_vectorizer.fit_transform(movies_df['genre'] + ' ' + movies_df['tags'])
+    
     # Calculate similarity using linear kernel
     user_matrix = tfidf_vectorizer.transform(user_data['genre'] + ' ' + user_data['tags'])
     cosine_similarities = linear_kernel(user_matrix, genres_tags_matrix).flatten()
@@ -56,4 +85,11 @@ def get_recommendations():
     return render_template('recommendations.html', recommendations=top_recommendations.to_dict('records'))
 
 if __name__ == '__main__':
+    # db_connection = mysql.connector.connect(
+    #     host='localhost',
+    #     user='root',
+    #     password='',
+    #     database='thirdeye'
+    # )
+    # print(db_connection)
     app.run(debug=True)
