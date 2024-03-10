b40aaf5df6c457034eac0718ce46872dd9c901ea
Jayanti2919 <jayanti2919@gmail.com>
2024-03-10 23:54:58 +0530

    landing hero area done

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,18 +2,65 @@ import React from "react";
 import ParticlesComponent from "../components/ParticleComponent";
 import "../App.css";
 import SideNav from "../components/SideNav";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
+  const nav = useNavigate();
+  const handleRedirect = (e, path) => {
+    e.preventDefault();
+    nav(path);
+  };
   return (
     <div className="bg-primary relative z-10">
       <div className="absolute">
-      <ParticlesComponent id="tsparticles"/>
+        <ParticlesComponent id="tsparticles" />
       </div>
-      <div className="h-screen md:flex items-center w-14 px-5 hidden">
-
-      <SideNav />
+      <div className="h-screen md:flex items-center px-5 hidden w-screen">
+        <SideNav />
+        <div className="flex flex-col text-secondary w-full items-center justify-center gap-10">
+          <img src="thirdeye_logo_white.svg" alt="thirdeye" />
+          <h1 className="uppercase alumni-sans text-5xl leading-[30px]">
+            third eye
+          </h1>
+          <div className="flex gap-10 mt-5">
+            <Button
+              variant="outlined"
+              sx={{
+                color: "#9D9FE2",
+                border: "1px solid #9D9FE2",
+                ":hover": {
+                  border: "1px solid #353B64",
+                  backgroundColor: "#353B64",
+                  color: "#F1EFEF",
+                },
+              }}
+              onClick={(e) => {
+                handleRedirect(e, "/login");
+              }}
+            >
+              sign in
+            </Button>
+            <Button
+              variant="outlined"
+              sx={{
+                color: "#9D9FE2",
+                border: "1px solid #9D9FE2",
+                ":hover": {
+                  border: "1px solid #353B64",
+                  backgroundColor: "#353B64",
+                  color: "#F1EFEF",
+                },
+              }}
+              onClick={(e) => {
+                handleRedirect(e, "/register");
+              }}
+            >
+              sign up
+            </Button>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 };
