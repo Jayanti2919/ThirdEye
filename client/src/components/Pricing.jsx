3d0e532563029af19c0c41b94698301c08b94e9e
NSinha025 <sinhanaman025@gmail.com>
2024-03-11 12:39:40 +0530

    added features and pricing component

---
+++ client/src/components/Pricing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pricing = () => {
+    return (
+        <div>
+            <h2>Pricing</h2>
+            {/* xxx */}
+        </div>
+    );
+};
+
+export default Pricing;
