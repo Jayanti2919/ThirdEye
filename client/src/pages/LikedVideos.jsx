1ccf16cb4c9cb7dca16458b093d966fcdd2b1658
Jayanti2919 <jayanti2919@gmail.com>
2024-03-17 17:08:18 +0530

    fixed folder structure

---
+++ client/src/pages/LikedVideos.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeSideNav from "../components/HomeSideNav";
+
+const LikedVideos = () => {
+    return (
+        <div className="bg-primary relative z-10 overflow-x-hidden">
+      <div className="absolute h-screen justify-center items-center flex left-10 px-5">
+        <HomeSideNav />
+      </div>
+      </div>
+    );
+};
+
+export default LikedVideos;
